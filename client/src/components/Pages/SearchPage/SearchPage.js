d4f1864997ae5cabe9fe863a4b13d5f962e060a2
Harim Jeong <harim97714@naver.com>
2021-11-25 11:09:57 +0900

    검색어 자동완성 로직 구현

--- client/src/components/Pages/SearchPage/SearchPage.js
+++ client/src/components/Pages/SearchPage/SearchPage.js
@@ -3,9 +3,11 @@ import { siteTitle } from '../../Config';
 import { withRouter } from 'react-router-dom';
 import './SearchPage.css';
 import { BsSearch } from "react-icons/bs";
+import { api_key } from '../../Config';
 
 function SearchPage() {
     const [SearchValue, setSearchValue] = useState("");
+    const [Items, setItems] = useState([]);
     
     const onSearchValueHandler = (event) => {
         setSearchValue(event.target.value);
@@ -17,8 +19,13 @@ function SearchPage() {
     }
     
     useEffect(() => {
+        const searchTo = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=ko-KR&query=${SearchValue}`;
         document.title = `영화 검색 - ${siteTitle}`;
-    }, [])
+
+        fetch(searchTo)
+            .then(res => res.json())
+            .then(res => setItems(res.results))
+    }, [SearchValue])
 
     return (
         <div>
@@ -44,6 +51,11 @@ function SearchPage() {
                         확인
                     </button>
                     </span>
+                </div>
+                <div>
+                    <ul>
+                        {Items && Items.map(item => (<li><a href={`/search/${item.title}`} style={{color: '#000000'}}>{item.title}</a></li>))}
+                    </ul>
                 </div>
                 </form>
             </div>
